990bb0ad692618f7549e3603d8e1f4d5c28f6da3
simodevjs <simodevjs@gmail.com>
2024-05-04 17:06:20 +0100

    222

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-# Using what you did in the task #0, extend
-# your Python script to export data in the CSV format.
+# Your Python script to export data in the CSV format.
 
 import requests
 import sys
 
 
 if __name__ == "__main__":
-    USER_ID = sys.argv[1]
-    jsonplaceholder = 'https://jsonplaceholder.typicode.com/users'
-    url = jsonplaceholder + '/' + USER_ID
-    response = requests.get(url)
+    user_id = sys.argv[1]
+    base_url = 'https://jsonplaceholder.typicode.com/users'
+    user_url = f'{base_url}/{user_id}'
+    response = requests.get(user_url)
     username = response.json().get('username')
-    todo_url = url + '/todos'
-    response = requests.get(todo_url)
+    todos_url = f'{user_url}/todos'
+    response = requests.get(todos_url)
     tasks = response.json()
-    with open(USER_ID + '.csv', 'w') as f:
+
+    with open(f'{user_id}.csv', 'w') as file:
         for task in tasks:
-            f.write('"{}","{}","{}","{}"\n'.format(USER_ID, username,
-                                                task.get('completed'),
-                                                task.get('title')))
+            line = f'"{user_id}","{username}","{task.get("completed")}","{task.get("title")}"\n'
+            file.write(line)
