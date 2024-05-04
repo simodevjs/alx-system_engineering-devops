320d689b3f1665e91ead8fb9463f86dac3bac73e
simodevjs <simodevjs@gmail.com>
2024-05-04 17:16:38 +0100

    222

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,33 +1,35 @@
 #!/usr/bin/python3
-# Using what you did in the task #0, extend your Python
-# script to export data in the JSON format.
-
+"""Export all employee tasks to a JSON file."""
 import json
 import requests
-import sys
+
+def fetch_data(url):
+    """Helper function to fetch data from the given URL."""
+    response = requests.get(url)
+    return response.json()
+
+def main():
+    url = "https://jsonplaceholder.typicode.com/"
+    users = fetch_data(f"{url}users")
+    todos = fetch_data(f"{url}todos")
+    
+    user_task_dict = {}
+
+    # Organize todos by user ID in the specified format
+    for user in users:
+        user_id = str(user['id'])
+        user_task_dict[user_id] = [
+            {
+                "username": user['username'],
+                "task": todo['title'],
+                "completed": todo['completed']
+            }
+            for todo in todos if todo['userId'] == user['id']
+        ]
+    
+    # Write the data to a JSON file
+    with open('todo_all_employees.json', 'w') as jsonfile:
+        json.dump(user_task_dict, jsonfile, indent=4)
 
 if __name__ == "__main__":
-    jsonplaceholder = 'https://jsonplaceholder.typicode.com/users'
-    response = requests.get(jsonplaceholder)
-    employees = response.json()
-    print(employees)
-    
-    data_dict = {}
-
-    for employee in employees:
-        USER_ID = employee.get('id')
-        username = employee.get('username')
-        url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(USER_ID)
-        response = requests.get(url)
-        tasks = response.json()
-        data_dict[USER_ID] = []
-        for task in tasks:
-            data_dict[USER_ID].append({
-                "username": username,
-                "task": task.get("title"),
-                "completed": task.get("completed"),
-            })
-
-    # Write the tasks data to a JSON file
-    with open('todo_all_employees.json', 'w') as f:
-        json.dump(data_dict, f)
+    main()
