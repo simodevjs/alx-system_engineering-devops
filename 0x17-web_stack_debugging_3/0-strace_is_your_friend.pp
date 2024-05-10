2835d0df04f3e0c7a597911e4bc1825c7dcf029c
simodevjs <simodevjs@gmail.com>
2024-05-10 01:50:42 +0100

    222

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,14 +1,45 @@
-file { '/etc/apache2/sites-available/000-default.conf':
-  ensure  => 'file',
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  content => template('apache/000-default.erb'), # You need to have this template in your Puppet module
-  notify  => Service['apache2'],
+# Puppet manifest to debug and fix common Apache issues
+class web_stack_debugging_3 {
+
+  # Ensure Apache is installed
+  package { 'apache2':
+    ensure => installed,
+  }
+
+  # Ensure the Apache service is running and enabled
+  service { 'apache2':
+    ensure    => running,
+    enable    => true,
+    require   => Package['apache2'], # Ensure package is installed before service starts
+    subscribe => File['/etc/apache2/apache2.conf', '/etc/apache2/sites-enabled/000-default.conf'],
+  }
+
+  # Ensure the main Apache config file exists and has correct permissions
+  file { '/etc/apache2/apache2.conf':
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+  }
+
+  # Ensure the default site configuration exists and has correct permissions
+  file { '/etc/apache2/sites-enabled/000-default.conf':
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    require => Package['apache2'], # Ensure Apache is installed before creating the file
+    notify  => Service['apache2'], # Restart Apache if this file changes
+  }
+
+  # Optional: Add a debug statement if you need to catch errors or log output
+  exec { 'debug_apache_config':
+    command     => "/usr/bin/strace -o /tmp/strace_apache.log -tt -e trace=all -p $(pgrep -o apache2)",
+    path        => '/usr/bin:/usr/sbin:/bin',
+    logoutput   => true,
+    refreshonly => true, # Only run when notified by another resource change
+    subscribe   => File['/etc/apache2/apache2.conf', '/etc/apache2/sites-enabled/000-default.conf'],
+  }
 }
 
-service { 'apache2':
-  ensure => 'running',
-  enable => true,
-}
-
+include web_stack_debugging_3
